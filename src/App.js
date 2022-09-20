aef696aef7133bb1f3a67b2fc687e0cabb4585c5
juanantunez <102978704+juanantunez@users.noreply.github.com>
2022-09-20 12:07:55 -0300

    primer envio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const nombre="Juan"
+  const apellido="Antunez"
+  let edad=25
+  console.log(edad)
+  edad=46
+  console.log(edad)
+
+  console.log(nombre, apellido)
+  console.log("hola "+nombre)
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Const - Let - var</h1>
+
+    </>
+    
   );
 }
 
